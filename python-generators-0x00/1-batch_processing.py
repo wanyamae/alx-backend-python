ed57cbb94168a17d60c940a20db1933d1c5657ab
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-07-06 15:36:25 +0300

    .

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -17,6 +17,7 @@ def stream_users_in_batches(batch_size):
         yield batch
     cursor.close()
     connection.close()
+    return
 
 def batch_processing(batch_size):
     for batch in stream_users_in_batches(batch_size):
