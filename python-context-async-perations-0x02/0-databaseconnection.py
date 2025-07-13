013d0e63742613aac28f7e0d622158e721bb1ff3
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-07-13 15:38:16 +0300

    Refactor DatabaseConnection to accept a customizable database name

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -1,7 +1,10 @@
 import sqlite3
 class DatabaseConnection:
+    def __init__(self, db_name='users.db'):
+        self.db_name = db_name
+
     def __enter__(self):
-        self.conn = sqlite3.connect('users.db')
+        self.conn = sqlite3.connect(self.db_name)
         return self.conn
 
     def __exit__(self, exc_type, exc_val, exc_tb):
