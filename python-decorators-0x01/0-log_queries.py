6d671fd708eb2ccd1815e73d21792d5e2dccf7e1
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-07-13 14:46:23 +0300

    .

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,18 +1,20 @@
+
+from datetime import datetime
 import sqlite3
 import functools
 
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        query = kwargs.get('query') if 'query' in kwargs else(args[0] if args else None)
-        print(f"Executing query: {query}")
+        query = kwargs.get('query') if 'query' in kwargs else (args[0] if args else None)
+        print(f"[{datetime.now()}] Executing query: {query}")
         return func(*args, **kwargs)
     return wrapper
 @log_queries
 def fetch_all_users(query):
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
-    cursor.execcute(query)
+    cursor.execute(query)
     results = cursor.fetchall()
     conn.close()
     return results
